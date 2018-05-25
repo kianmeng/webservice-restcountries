a0daba5aa514e2541b352c2148e92900b91009c2
Kian Meng, Ang <kianmeng@cpan.org>
2018-05-26 00:22:29 +0800

    Initial import

---
+++ README.md
@@ -0,0 +1,25 @@
+# NAME
+
+WebService::RESTCountries - A Perl module to interface with the REST Countries
+(restcountries.eu) webservice.
+
+# SYNOPSIS
+
+    use WebService::RESTCountries;
+
+# DESCRIPTION
+
+WebService::RESTCountries is a Perl client helper library for the REST
+Countries API (restcountries.eu).
+
+# COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2017 by Kian Meng, Ang.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
+
+# AUTHOR
+
+Kian Meng, Ang <kianmeng@users.noreply.github.com>
---
+++ lib/WebService/RESTCountries.pm
@@ -0,0 +1,38 @@
+package WebService::RESTCountries;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::RESTCountries - A Perl module to interface with the REST Countries
+(restcountries.eu) webservice.
+
+=head1 SYNOPSIS
+
+  use WebService::RESTCountries;
+
+=head1 DESCRIPTION
+
+WebService::RESTCountries is a Perl client helper library for the REST
+Countries API (restcountries.eu).
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2017 by Kian Meng, Ang.
+
+This is free software, licensed under:
+
+    The Artistic License 2.0 (GPL Compatible)
+
+=head1 AUTHOR
+
+Kian Meng, Ang E<lt>kianmeng@users.noreply.github.comE<gt>
+
+=cut
