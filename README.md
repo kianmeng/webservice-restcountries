b1df695ef23cef6d47f197a1ad22d358f46a57ee
Kian-Meng, Ang <kianmeng@cpan.org>
2019-04-28 23:56:04 +0800

    Tidy up contributing doc

--- README.md
+++ README.md
@@ -23,38 +23,7 @@ Countries API (restcountries.eu).
 
 Source repository at [https://github.com/kianmeng/webservice-restcountries](https://github.com/kianmeng/webservice-restcountries).
 
-## Docker
-
-If you have Docker installed, you can build your Docker container for this
-project.
-
-    $ docker build -t webservice-restcountries .
-    $ docker run -it -v $(pwd):/root webservice-restcountries bash
-    # cpanm --installdeps --notest .
-
-## Milla
-
-Setting up the required packages.
-
-    $ milla authordeps --missing | cpanm
-    $ milla listdeps --missing | cpanm
-
-Check you code coverage.
-
-    $ milla cover
-
-Several ways to run the test.
-
-    $ milla test
-    $ milla test --author --release
-    $ AUTHOR_TESTING=1 RELEASE_TESTING=1 milla test
-    $ AUTHOR_TESTING=1 RELEASE_TESTING=1 milla run prove t/01_instantiation.t
-    $ LOGGING=1 milla run prove t/t/02_request.t
-
-Release the module.
-
-    $ milla build
-    $ milla release
+How to contribute? Follow through the [CONTRIBUTING.md](https://github.com/kianmeng/webservice-restcountries/blob/master/CONTRIBUTING.md) document to setup your development environment.
 
 # METHODS
 
--- lib/WebService/RESTCountries.pm
+++ lib/WebService/RESTCountries.pm
@@ -238,38 +238,7 @@ Countries API (restcountries.eu).
 
 Source repository at L<https://github.com/kianmeng/webservice-restcountries|https://github.com/kianmeng/webservice-restcountries>.
 
-=head2 Docker
-
-If you have Docker installed, you can build your Docker container for this
-project.
-
-    $ docker build -t webservice-restcountries .
-    $ docker run -it -v $(pwd):/root webservice-restcountries bash
-    # cpanm --installdeps --notest .
-
-=head2 Milla
-
-Setting up the required packages.
-
-    $ milla authordeps --missing | cpanm
-    $ milla listdeps --missing | cpanm
-
-Check you code coverage.
-
-    $ milla cover
-
-Several ways to run the test.
-
-    $ milla test
-    $ milla test --author --release
-    $ AUTHOR_TESTING=1 RELEASE_TESTING=1 milla test
-    $ AUTHOR_TESTING=1 RELEASE_TESTING=1 milla run prove t/01_instantiation.t
-    $ LOGGING=1 milla run prove t/t/02_request.t
-
-Release the module.
-
-    $ milla build
-    $ milla release
+How to contribute? Follow through the L<CONTRIBUTING.md|https://github.com/kianmeng/webservice-restcountries/blob/master/CONTRIBUTING.md> document to setup your development environment.
 
 =head1 METHODS
 
